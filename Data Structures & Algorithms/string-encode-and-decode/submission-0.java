35cd9c946054d2bedd9d0953a57654a0b140fadf
PRIYUNSHU21 <priyunshu.cse123096@bppimt.ac.in>
2026-04-16 11:52:24 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,37 @@
+class Solution {
+
+/*
+    what we have to do:
+    > we will have a list of strings given to us
+    > we will amalgamate the strings into one large string in the format {number#string}
+    > The special character helps determine the end and  start of new strings while number is the number of characters
+    > we will use string builder for the string amalgamation (it reserves a small pool of space thus making the string mutable)
+*/
+    public String encode(List<String> strs) {
+
+        StringBuilder build = new StringBuilder();//playground for mutation and manipulation
+
+        for(String unit : strs)
+        {
+            build.append(unit.length() + "#" + unit);
+        }
+
+        return build.toString(); //making the string immutable
+
+    }
+
+    public List<String> decode(String str) {
+
+        List<String> result = new ArrayList<>();
+        int i= 0;
+
+        while(str.charAt(i) != '#')
+        {
+            i++;
+        }
+        int j = i;
+
+        int length = j - i;
+
+    }
+}
