31a3f2798fd01d93f7f29a9bd24bc29c053c01ae
PRIYUNSHU21 <priyunshu.cse123096@bppimt.ac.in>
2026-04-25 19:09:21 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int trap(int[] height) {
+        
+        int counter = 0;
+        int lmax = height[0];
+        int rmax = height[height.length - 1];
+        int l = 0;
+        int r = height.length -1;
+
+        while(l<r)
+        {
+            if(lmax <= rmax)
+            {
+                if(lmax >= height[l])
+                {
+                    counter += lmax - height[l];
+                    l++;
+                    lmax = Math.max(lmax, height[l]);
+                
+                }
+                /*else
+                {
+                    lmax = height[l];
+                    l++;
+                }*/
+            }
+            else
+            {
+                if(rmax >= height[r])
+                {
+                    counter += rmax - height[r];
+                    r--;
+                    rmax = Math.max(rmax, height[r]);
+                
+                }
+               /* else
+                {
+                    rmax = height[r];
+                    r++;
+                }*/
+            }
+        }
+
+        return counter;
+    }
+}
