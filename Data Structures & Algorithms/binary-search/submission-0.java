d20810037b37fa614c38e5af1587e9244f7ca8c7
PRIYUNSHU21 <priyunshu.cse123096@bppimt.ac.in>
2026-07-26 19:24:32 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+       int result = Arrays.binarySearch(nums,target);
+
+       if(result < 0)
+       {
+          return -1;
+       }
+       else
+       {
+         return result;
+       }
+    }
+}
