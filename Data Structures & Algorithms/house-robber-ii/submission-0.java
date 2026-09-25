5bdacb8a2b3bcaae5f7a633822a39ca86c58d4e3
PRIYUNSHU21 <priyunshu.cse123096@bppimt.ac.in>
2026-09-26 00:13:33 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        if(nums.length == 0)return 0;
+        if(nums.length == 1)return nums[0];
+
+        int[] dp = new int[nums.length + 2];
+
+        for(int i = nums.length - 2; i >= 0; i--)
+        {
+            dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+        }
+
+        int max_so_far = dp[0];
+
+         for(int i = nums.length - 1; i >= 1; i--)
+        {
+            dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+        }
+
+        return Math.max(dp[1], max_so_far);
+        
+    }
+}
